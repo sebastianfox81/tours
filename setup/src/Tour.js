ba8feba22c502b93916676e2cbcd86b98d6aaec1
Sebastian <sebastianfox81@gmail.com>
2022-01-19 17:41:39 -0500

    Created toggle functionality for tour info

--- setup/src/Tour.js
+++ setup/src/Tour.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, price, name }) => {
 
+  const [ readMore, setReadMore ] = useState(false);
 
   return (
     <article className="single-tour">
@@ -11,8 +12,11 @@ const Tour = ({ id, image, info, price, name }) => {
         <h4>{name}</h4>
         <h4 className="tour-price">{price}</h4>
       </div>
-      <p>{info}</p>
-      <button className="delete-btn">
+      <p>{readMore ? info : `${info.substring(0, 200)}...` }</p>
+      <button onClick={() => setReadMore(!readMore)}>
+        {readMore ? ' show less' : ' read more'}
+      </button>
+      <button className="delete-btn" onClick={}>
         Not interested
       </button>
     </footer>
