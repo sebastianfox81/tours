23529b878331c5b3ba7696192de46c412152c700
Sebastian <sebastianfox81@gmail.com>
2022-01-19 16:40:00 -0500

    Implemented fetch tours functionality

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,11 +1,38 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project</h2>
+
+  const [ loading, setLoading ] = useState(true);
+  const [ tours, setTours ] = useState([]);
+
+  const fetchTours = () => {
+    setLoading(true);
+    axios.get(url)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, []);
+
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  )
 }
 
 export default App
